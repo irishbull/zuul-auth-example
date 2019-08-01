b0e6c45c15625d358872530908830a3e8ff9d8d3
Begi <g.gissi@reply.it>
2019-08-01 16:12:57 +0200

    User Attempt Entity

--- common/src/main/java/Entity/TransactionLoginEntity.java
+++ common/src/main/java/Entity/TransactionLoginEntity.java
@@ -1,4 +1,21 @@
 package Entity;
 
-public class TransactionLoginEntity {
+import Base.BaseEntity;
+import Enum.LoginStateEnum;
+import javax.persistence.*;
+
+public class TransactionLoginEntity extends BaseEntity {
+    @Id
+    @Column(name = "USER_ID", nullable = false)
+    private Long uid;
+    @Column(name = "OTP", nullable = false)
+    private String otp;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "LOGIN_STATE", nullable = false)
+    private LoginStateEnum loginState;
+    @Column(name = "ACTUAL_ATTEMPTS", nullable = false)
+    private int attemptsCounter;
+
+    public TransactionLoginEntity() {
+    }
 }
